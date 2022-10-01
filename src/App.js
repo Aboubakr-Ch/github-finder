085c4fa0f965d953132447709de0ff9d477912f7
Abou-Bakr <chikouche.aboubakr@gmail.com>
2022-10-01 13:55:43 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {Fragment, Component} from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios'
+
+class App extends Component {
+  state = {
+    users:[],
+    loading: false
+  }
+  async componentDidMount() {
+    this.setState({loading: true});
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({users: res.data, loading: false});
+  }
+
+  render(){
+    return (
+      <Fragment>
+        <Navbar title='Github Finder' icon='fab fa-github'></Navbar>
+        <div className='container'>
+        <Users loading={this.state.loading} users={this.state.users}></Users>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
